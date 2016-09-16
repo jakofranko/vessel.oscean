3f1579500ebc874b029e56950aeaa65a18e7a3bc
Devine Lu Linvega <aliceffekt@gmail.com>
2016-09-16 11:58:36 -0700

    Migrated to @actor/@target

--- vessel.rb
+++ vessel.rb
@@ -8,55 +8,63 @@ require 'date'
 $timeStart = Time.new
 
 Dir["#{$vessel_path}/inc/objects/*"].each do |file_name|
-	load(file_name)
+  load(file_name)
 end
 
 class Oscea
 
-	def http q = "Home"
+  class Actions
 
-		@query = q != "" ? q.gsub("+"," ").split(":").first : "Home"
-		@module = q.include?(":") ? q.gsub("+"," ").split(":").last : ""
+    include ActionCollection
 
-		@data = {
-		  "topic"   => @query,
-		  "module"  => @module,
-		  "lexicon" => En.new("lexicon").to_h,
-		  "horaire" => Di.new("horaire").to_a
-		}
+    def http q = "Home"
 
-		@page   = Page.new(@data)
-		@layout = Layout.new(@page)
+      @query = q != "" ? q.gsub("+"," ").split(":").first : "Home"
+      @module = q.include?(":") ? q.gsub("+"," ").split(":").last : ""
 
-		$photo = @page.diary ? @page.diary.photo : nil
+      @data = {
+        "topic"   => @query,
+        "module"  => @module,
+        "lexicon" => En.new("lexicon").to_h,
+        "horaire" => Di.new("horaire").to_a
+      }
 
-		return "
-		<!DOCTYPE html>
-		<html> 
-		<head>
-			<meta charset='UTF-8'>
-			<meta name='viewport'            content='width=device-width, initial-scale=1, maximum-scale=1'>
-			<meta name='apple-mobile-web-app-capable' content='yes' />
-			<meta name='viewport' 			 content='width=device-width, initial-scale=1.0'>
-			<meta name='description'         content='Works of Devine Lu Linvega' />
-			<meta name='keywords'            content='aliceffekt, traumae, ikaruga, devine lu linvega' />
-		  	<meta name='apple-mobile-web-app-capable' content='yes'>
+      @page   = Page.new(@data)
+      @layout = Layout.new(@page)
 
-			<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js' ></script>
+      $photo = @page.diary ? @page.diary.photo : nil
 
-			<link rel='shortcut icon' 		 href='http://wiki.xxiivv.com/img/interface/favicon.ico' />
-			<link rel='apple-touch-icon-precomposed' 	 href='../../img/interface/phone_xxiivv.png'/>
-			<link rel='shortcut icon' href='http://wiki.xxiivv.com/img/interface/favicon.ico' />
+      return "
+      <!DOCTYPE html>
+      <html> 
+      <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport'            content='width=device-width, initial-scale=1, maximum-scale=1'>
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='viewport'        content='width=device-width, initial-scale=1.0'>
+        <meta name='description'         content='Works of Devine Lu Linvega' />
+        <meta name='keywords'            content='aliceffekt, traumae, ikaruga, devine lu linvega' />
+          <meta name='apple-mobile-web-app-capable' content='yes'>
 
-			<link rel='stylesheet'           type='text/css'                 href='inc/style.reset.css' />
-			<link rel='stylesheet'           type='text/css'                 href='inc/style.main.css' />
-			<script 														 src='inc/jquery.main.js'></script>
+        <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js' ></script>
 
-			<title>"+@layout.title+"</title>
-		</head>
-		<body>"+@layout.view+"</body>
-		</html>
-		"
-	end
+        <link rel='shortcut icon'      href='http://wiki.xxiivv.com/img/interface/favicon.ico' />
+        <link rel='apple-touch-icon-precomposed'   href='../../img/interface/phone_xxiivv.png'/>
+        <link rel='shortcut icon' href='http://wiki.xxiivv.com/img/interface/favicon.ico' />
+
+        <link rel='stylesheet'           type='text/css'                 href='inc/style.reset.css' />
+        <link rel='stylesheet'           type='text/css'                 href='inc/style.main.css' />
+        <script                              src='inc/jquery.main.js'></script>
+
+        <title>"+@layout.title+"</title>
+      </head>
+      <body>"+@layout.view+"</body>
+      </html>
+      "
+    end
+
+  end
+  
+  def actions ; return Actions.new(self,self) end
 
 end
