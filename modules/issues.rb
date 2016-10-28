cca17e0fd95159bfe59f2bc22a30317156ea4e4e
Devine Lu Linvega <aliceffekt@gmail.com>
2016-10-28 15:23:46 -0700

    Added a issues module view

--- modules/issues.rb
+++ modules/issues.rb
@@ -5,13 +5,22 @@ class CorpseHttp
 
   def view
 
-    html = term.long.runes+"\n"
-    
-    html += "<h2>Tasks</h2>\n"
-    term.tasks.to_a.each do |task|
-      html += "<ln>#{task}</ln>\n"
-    end
+    html = ""
 
+    if term.like("home") || term.like("issues")
+      term.tasks.to_a.each do |topic,tasks|
+        html += "<h2>#{topic}</h2>\n"
+        tasks.each do |task|
+          html += "<ln>#{task}</ln>\n"
+        end
+      end
+    else
+      html += "<h2>Tasks</h2>\n"
+      term.tasks.to_a.each do |task|
+        html += "<ln>#{task}</ln>\n"
+      end
+    end
+    
     return "<wr>#{html}</wr>"
 
   end
